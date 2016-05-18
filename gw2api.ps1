4d91b8c6d8b8c9371083ae20bcddb3664bad04af
O'Ryan Hedrick <ryebread8403@gmail.com>
2016-05-18 15:39:31 -0500

    initial commit. Able to query prices of items in trading post.

---
+++ gw2api.ps1
@@ -0,0 +1,42 @@
+﻿<#
+.Synopsis
+   Retrieve data from the Guild Wars 2 API
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-fromgw2api
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$false,
+                   Position=0)]
+        $endpoint,
+
+        # Param2 help description
+        [parameter(mandatory=$false,position=1)][string]
+        $ids = $null
+    )
+
+    Begin
+    {
+    $baseapiurl = "https://api.guildwars2.com/v2/"
+    if ($endpoint -eq "prices") {$suffix = "commerce/prices"}
+    if ($ids -ne $null) {$parameter = "?ids=" + [system.string]::join(",",$ids)} 
+    }
+    Process
+    {
+    Invoke-RestMethod -Uri ($baseapiurl + $suffix + $parameter)
+    }
+    End
+    {
+    }
+}
